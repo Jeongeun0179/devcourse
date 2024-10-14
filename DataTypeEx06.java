93ea9e7eb068b71dc3c0391f2904a65daf2a93ed
jeongeunlee <dearellen@yonsei.ac.kr>
2024-10-14 18:23:04 +0900

    first commit

---
+++ DataTypeEx06.java
@@ -0,0 +1,24 @@
+public class DataTypeEx06 {
+    public static void main(String[] args) {
+        // 실수 
+        // float, double(*)
+
+        //int i = 2.5;
+        //System.out.println( i ); => incompatible types: possible lossy conversion
+
+        //float f = 2.5;
+        //System.out.println( f );
+
+        float f = 2.5f;
+        System.out.println( f );
+
+        double d = 2.5;
+        System.out.println( d );
+
+        // 정수 => 실수 => 지수
+        // 1000 -> 1.0e3
+        double d2 = 1.0e3;
+        System.out.println( d2 );
+
+    }
+}
